e69d2b773258bc0345dc55234c04b37c6e68689a
Ivan Galarza <ivang@bluetrailsoft.com>
2022-02-25 10:32:42 -0400

    change video to dowload and adding more request

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post, StreamableFile } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, StreamableFile } from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { join } from 'path';
+import {v4 as uuidv4} from 'uuid'
 
 @Controller()
 export class TestingController {
@@ -18,8 +19,8 @@ export class TestingController {
   @Post("/realtime")
   initRealTime() {
     return {
-      "filename": "moonVideo.mp4",
-      "filepath": "src/files/moonVideo.mp4",
+      "filename": "video1.mp4",
+      "filepath": "src/files/video1.mp4",
       "date": 1624436412,
       "fps": 60,
       "resolution": "1920x1080",
@@ -37,7 +38,49 @@ export class TestingController {
   }
 
   // Delete video
+  @Delete("realtime_storage/:id")
+  deleteRecord(@Param('id') uuid: string) {
+    if (uuid == "1158de31-c840-48b9-8bfb-941a1b65cccb") {
+      return "ok"
+    } else {
+      return "no"
+    }
+  }
 
+  @Get("realtime_storage/:id")
+  getRecordingInfo(@Param('id') uuid: string) {
+    let myuuid = uuidv4();
+    return {
+      "filename": "video1.mp4",
+      "filepath": "src/files/video1.mp4",
+      "date": 1624436412,
+      "fps": 60,
+      "resolution": "1920x1080",
+      "uuid": `${myuuid}`,
+      "frames": 0,
+      "thumb_path": "hand_ring2.png",
+      "ring_id": "N4246000_sol_des"
+    }
+  }
+
+  @Get("realtime_storage")
+  getRecordingList() {
+    return {
+      "videos": [
+        {
+          "filename": "video1.mp4",
+          "filepath": "src/files/video1.mp4",
+          "date": 1624436412,
+          "fps": 60,
+          "resolution": "1920x1080",
+          "uuid": "1158de31-c840-48b9-8bfb-941a1b65cccb",
+          "frames": 0,
+          "thumb_path": "hand_ring2.png",
+          "ring_id": "N4246000_sol_des"
+        }
+      ]
+    }
+  }
 
 
 
