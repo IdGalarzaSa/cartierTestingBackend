3ed3943030ae9cce816037f89999daa61891ea8b
Ivan Galarza <ivang@bluetrailsoft.com>
2022-03-09 11:45:58 -0400

    testing videos

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Delete, Get, Param, Post, StreamableFile } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, StreamableFile } from '@nestjs/common';
 import { createReadStream } from 'fs';
 import { join } from 'path';
+import {v4 as uuidv4} from 'uuid'
 
 @Controller()
 export class TestingController {
@@ -12,22 +13,71 @@ export class TestingController {
   }
 
 
-  // Cartier services
+  // // Cartier services
+
+
+  // @Get("/data/videos/:uuid/rings/:ringID")
+  // downloadVideo(@Param('uuid') uuid: string, @Param('ringID') ringId: string ) {
+  //   const file = createReadStream(join(process.cwd(),`src/files/${filepath}`));
+  //   return new StreamableFile(file)
+  // }
+
+
+  @Get("/home/lookingglass/data/ios_app/rings_info.json")
+  getRings() {
+    return {
+      "rings": [
+        {
+          "ring_id": "CRN4187352_love_solitaire",
+          "name": "LOVE SOLITAIRE",
+          "reference_number": "CRN4187352",
+          "description": "LOVE solitaire, rose gold 750/1000, set with a brilliant-cut diamond available from 0.23 to 0.49 carats.",
+          "ring_image": "data/ios_app/ALG_Ring_Love-Solitaire.png",
+          "category": "Fine Jewelry",
+          "catalog_url": "https://www.cartier.com/en-us/jewelry/engagement-rings/love-solitaire-CRN4250100.html"
+        },
+        {
+          "ring_id": "CRN4773752_c_decartier_solitaire",
+          "name": "C DE CARTIER SOLITAIRE",
+          "reference_number": "CRN4773752",
+          "description": "LOVE solitaire, rose gold 750/1000, set with a brilliant-cut diamond available from 0.23 to 0.49 carats.",
+          "ring_image": "data/ios_app/ALG_Ring_C-de-Cartier-Solitaire.png",
+          "category": "TK",
+          "catalog_url": "NA"
+        }
+      ]
+    }
+  }
+  
 
   // Init streaming
   @Post("/realtime")
-  initRealTime() {
+  initRealTime(@Body() body) {
+
+    // console.log(`Recording: ${JSON.stringify(body)}`)
+
     return {
-      "filename": "moonVideo.mp4",
-      "filepath": "sample1.mp4",
-      "date": 1624436412,
-      "fps": 60,
-      "resolution": "1920x1080",
-      "uuid": "1158de31-c840-48b9-8bfb-941a1b65cccb",
-      "frames": 0,
-      "thumb_path": "demoRing1.png",
-      "ring_id": "N4246000_sol_des"
+      "filename":"",
+      "filepath":"",
+      "date":0,
+      "fps":60,
+      "resolution":"",
+      "uuid":"d8102b97-40d5-4354-8517-2e954e681467",
+      "frames":0,
+      "thumb_path":"",
+      "rings":[]
     }
+    // {
+    //   "filename": "moonVideo.mp4",
+    //   "filepath": "sample1.mp4",
+    //   "date": 1624436412,
+    //   "fps": 60,
+    //   "resolution": "1920x1080",
+    //   "uuid": "1158de31-c840-48b9-8bfb-941a1b65cccb",
+    //   "frames": 0,
+    //   "thumb_path": "demoRing1.png",
+    //   "ring_id": "N4246000_sol_des"
+    // }
   }
 
   // Stop streaming
@@ -39,11 +89,7 @@ export class TestingController {
   // Delete video
   @Delete("realtime_storage/:id")
   deleteRecord(@Param('id') uuid: string) {
-    if (uuid == "1158de31-c840-48b9-8bfb-941a1b65cccb") {
-      return "ok"
-    } else {
-      return "no"
-    }
+    return "ok"
   }
 
   @Get("realtime_storage/:id")
@@ -55,7 +101,7 @@ export class TestingController {
       "date": 1624436412,
       "fps": 60,
       "resolution": "1920x1080",
-      "uuid": `1158de31-c840-48b9-8bfb-941a1b65cccb`,
+      "uuid": "d8102b97-40d5-4354-8517-2e954e681467",
       "frames": 0,
       "thumb_path": "demoRing1.png",
       "ring_id": "N4246000_sol_des"
@@ -72,7 +118,7 @@ export class TestingController {
           "date": 1624436412,
           "fps": 60,
           "resolution": "1920x1080",
-          "uuid": "1158de31-c840-48b9-8bfb-941a1b65cccb",
+          "uuid": "d8102b97-40d5-4354-8517-2e954e681467",
           "frames": 0,
           "thumb_path": "demoRing1.png",
           "ring_id": "N4246000_sol_des"
