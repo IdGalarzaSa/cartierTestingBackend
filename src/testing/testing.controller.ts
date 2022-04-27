406b1c974875e80a2dbfc7854915eeaa331180a3
Ivan Galarza <ivang@bluetrailsoft.com>
2022-04-27 04:00:37 -0400

    realtime mocked response

--- src/testing/testing.controller.ts
+++ src/testing/testing.controller.ts
@@ -31,41 +31,41 @@ export class TestingController {
         "ring_id": "anillo1",
         "name": "Ring 1",
         "reference_number": "CRN4187351",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum - 1.",
+        "description": "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum - 1.",
         "category": "Fine Jewelry",
         "catalog_url": "https://www.cartier.com/en-us/jewelry/engagement-rings/love-solitaire-CRN4250100.html",
         "ring_image": "/data/images/ring1/ring1-S.png",
         "ring_image_hd": "/data/images/ring1/ring1-XL.png",
         "overlay_image_url": "/data1/rings/N4246000_sol_des16/3a772893-a66c-4857-aa3e-eecc27440655",
-        "updated": 1650877083,
+        "updated": 1650877041,
         "id": 1
       },
       {
         "is_deleted": false,
         "ring_id": "anillo2",
-        "name": "Ring 1",
+        "name": "Ring 2",
         "reference_number": "CRN4187352",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum - 2.",
+        "description": "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum - 2.",
         "category": "Fine Jewelry",
         "catalog_url": "https://www.cartier.com/en-us/jewelry/engagement-rings/love-solitaire-CRN4250100.html",
         "ring_image": "/data/images/ring2/ring2-S.png",
         "ring_image_hd": "/data/images/ring2/ring2-XL.png",
         "overlay_image_url": "/data1/rings/N4246000_sol_des16/3a772893-a66c-4857-aa3e-eecc27440655",
-        "updated": 1650877040,
+        "updated": 1650877042,
         "id": 2
       },
       {
         "is_deleted": false,
         "ring_id": "anillo3",
-        "name": "Ring 1",
+        "name": "Ring 3",
         "reference_number": "CRN4187353",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum - 3.",
+        "description": "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum - 3.",
         "category": "Fine Jewelry",
         "catalog_url": "https://www.cartier.com/en-us/jewelry/engagement-rings/love-solitaire-CRN4250100.html",
         "ring_image": "/data/images/ring3/ring3-S.png",
         "ring_image_hd": "/data/images/ring3/ring3-L.png",
         "overlay_image_url": "/data1/rings/N4246000_sol_des16/3a772893-a66c-4857-aa3e-eecc27440655",
-        "updated": 1650877041,
+        "updated": 1650877043,
         "id": 3
       },
       // {
@@ -104,31 +104,45 @@ export class TestingController {
   @Post("/realtime")
   initRealTime(@Body() body) {
 
-    if (body.ring_id == "CRN4187352_love_solitaire_real_time") {
-      return {
-        "filename":"",
-        "filepath":"",
-        "date":0,
-        "fps":60,
-        "resolution":"",
-        "uuid":"d8102b97-40d5-4354-8517-2e954e681467",
-        "frames":0,
-        "thumb_path":"",
-        "rings":[]
-      }
-    } else {
-      return {
-        "filename":"",
-        "filepath":"",
-        "date":0,
-        "fps":60,
-        "resolution":"",
-        "uuid": "1158de31-c840-48b9-8bfb-941a1b65cccb",
-        "frames":0,
-        "thumb_path":"",
-        "rings":[]
-      }
+    console.log(body)
+
+    return {
+      "filename": "moonVideo.mp4",
+      "filepath": "sample1.mp4",
+      "date": 1624436412,
+      "fps": 60,
+      "resolution": "1920x1080",
+      "uuid": "d8102b97-40d5-4354-8517-2e954e681467",
+      "frames": 0,
+      "thumb_path": "demoRing1.png",
+      "ring_id": "N4246000_sol_des"
     }
+
+    // if (body.ring_id == "CRN4187352_love_solitaire_real_time") {
+    //   return {
+    //     "filename":"",
+    //     "filepath":"",
+    //     "date":0,
+    //     "fps":60,
+    //     "resolution":"",
+    //     "uuid":"d8102b97-40d5-4354-8517-2e954e681467",
+    //     "frames":0,
+    //     "thumb_path":"",
+    //     "rings":[]
+    //   }
+    // } else {
+    //   return {
+    //     "filename":"",
+    //     "filepath":"",
+    //     "date":0,
+    //     "fps":60,
+    //     "resolution":"",
+    //     "uuid": "1158de31-c840-48b9-8bfb-941a1b65cccb",
+    //     "frames":0,
+    //     "thumb_path":"",
+    //     "rings":[]
+    //   }
+    // }
   }
 
   // Stop streaming
